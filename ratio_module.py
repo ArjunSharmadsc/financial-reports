57db73fae65200a6eb8d4f3d5f3a5bdf032848fa
ArjunSharmadsc <arjun2005sh@gmail.com>
2026-07-17 00:04:42 +0530

    added quick ratio

--- ratio_module.py
+++ ratio_module.py
@@ -8,6 +8,38 @@ class lquidity_ratio:
         self.current_asset = current_asset
         self.current_liability = current_liability
         
-        result = current_asset/current_liability
+        result = self.current_asset/self.current_liability
+
+        if result >= 3:
+            print( f"current ratio is {result} which is way to high and strong" )
+
+        elif result < 3 and result > 2:
+            print( f"current ratio is {result} which is high and strong but feisible to sustain long term" )
+
+        else:
+            print( f"current ratio is {result} which is low and concerning" )
+
         return result
+    
+    def quick_ratio(self, quick_asset, current_liability):
+        self.quick_asset = quick_asset
+        self.current_liability = current_liability
+
+        qr_result = self.quick_asset/ self.current_liability
+
+        if qr_result >= 1:
+            print( f"current ratio is {qr_result} which is way to high and strong" )
+
+        elif qr_result < 1 and qr_result > 0 :
+            print( f"current ratio is {qr_result} which is high and strong but feisible to sustain long term" )
+
+        else:
+            print( f"current ratio is {qr_result} which is low and concerning" )
+
+        return qr_result
+    
+    
+    
+
+
 
