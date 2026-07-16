0c3c685882e52c8cbcb84e071046719cd80fdd5e
ArjunSharmadsc <arjun2005sh@gmail.com>
2026-07-16 23:51:59 +0530

    current ratio

--- ratio_module.py
+++ ratio_module.py
@@ -1,2 +1,13 @@
 import pandas as pd
 import numpy as np
+import math
+
+class lquidity_ratio:
+
+    def current_ratio(self,current_asset, current_liability):
+        self.current_asset = current_asset
+        self.current_liability = current_liability
+        
+        result = current_asset/current_liability
+        return result
+
