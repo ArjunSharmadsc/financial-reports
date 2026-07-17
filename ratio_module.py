a08298f95968446cc715b780f35a74d83c03a080
ArjunSharmadsc <arjun2005sh@gmail.com>
2026-07-17 14:38:59 +0530

    rectified logical error and refined flow

--- ratio_module.py
+++ ratio_module.py
@@ -2,39 +2,48 @@ import pandas as pd
 import numpy as np
 import math
 
-class lquidity_ratio:
+class lquidityratio:
 
     def current_ratio(self,current_asset, current_liability):
         self.current_asset = current_asset
         self.current_liability = current_liability
+
+        if current_liability == 0:
+            raise ValueError("Current liability cannot be zero.\n")
         
         result = self.current_asset/self.current_liability
 
-        if result >= 3:
-            print( f"current ratio is {result} which is way to high and strong" )
+        if result < 1:
+            print( f"current ratio is {result:.2f} which is way to very low and there are liquidity concerns \n" )
 
-        elif result < 3 and result > 2:
-            print( f"current ratio is {result} which is high and strong but feisible to sustain long term" )
+        elif 1 <= result < 2:
+            print( f"current ratio is {result:.2f} which is high and and healthy \n" )
+
+        elif 2 <= result < 3:
+            print( f"current ratio is {result:.2f} which is high and strong but feisible to sustain long term \n" )
 
         else:
-            print( f"current ratio is {result} which is low and concerning" )
+            print( f"current ratio is {result:.2f} which is very high indicating idle assets or inefficient use of working capital \n" )
 
         return result
     
     def quick_ratio(self, quick_asset, current_liability):
         self.quick_asset = quick_asset
         self.current_liability = current_liability
+       
+        if current_liability == 0:
+             raise ValueError("Current liability cannot be zero. \n")
 
         qr_result = self.quick_asset/ self.current_liability
 
-        if qr_result >= 1:
-            print( f"current ratio is {qr_result} which is way to high and strong" )
+        if qr_result >= 2:
+            print( f"quick ratio is {qr_result:.2f}, meaning there is excess cash or receivables  \n " )
 
-        elif qr_result < 1 and qr_result > 0 :
-            print( f"current ratio is {qr_result} which is high and strong but feisible to sustain long term" )
+        elif 1 <= qr_result < 2:
+            print( f"quick ratio is {qr_result:.2f} which is high and strong but feasible to sustain long term \n" )
 
         else:
-            print( f"current ratio is {qr_result} which is low and concerning" )
+            print( f"quick ratio is {qr_result:.2f} which is low and concerning \n" )
 
         return qr_result
     
